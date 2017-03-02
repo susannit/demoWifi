ccc4fc9202c9f121244f14eecdf5f1c56abf34e6
susannit <sudharsansalem@gmail.com>
2017-03-01 22:12:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -31,3 +31,16 @@ var gateway = braintree.connect({
   var server = app.listen(process.env.PORT || 8080, function () {
     console.log("App now running on port", server.address().port);
   });
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+var database = mongodb.MongoClient.connect(process.env.MONGOLAB_AMBER_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
+
+  // Save database object from the callback for reuse.
+db = database;
