57e16ec0c9bcdb71230d23b316a0345ffaf086e5
susannit <sudharsansalem@gmail.com>
2017-03-16 14:41:54 -0700

    Update gateway.js

--- gateway.js
+++ gateway.js
@@ -1,17 +1,27 @@
 'use strict';
 
 var braintree = require('braintree');
-var environment, gateway;
+var environment, gateway, getToken;
 
 /**
  * Instantiate your gateway (update here with your Braintree API Keys)
  */
-var environment = process.env.BT_ENVIRONMENT.charAt(0).toUpperCase() + process.env.BT_ENVIRONMENT.slice(1);
-var gateway = braintree.connect({
+environment = process.env.BT_ENVIRONMENT.charAt(0).toUpperCase() + process.env.BT_ENVIRONMENT.slice(1);
+gateway = braintree.connect({
   environment:  braintree.Environment[environment],
   merchantId:   process.env.BT_MERCHANT_ID,
   publicKey:    process.env.BT_PUBLIC_KEY,
   privateKey:   process.env.BT_PRIVATE_KEY
 });
 
+getToken=function(req,res){
+    gateway.clientToken.generate({}, function (err, res) {
+      if (err) throw err;
+      res.json({
+        "client_token": res.clientToken
+      });
+	  });
+}
+
 module.exports = gateway;
+module.exports = getToken;
